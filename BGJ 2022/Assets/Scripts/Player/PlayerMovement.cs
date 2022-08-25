5950b5b054e8274b65f85823d8cc18ce849cdcf7
Chiagozie Ogbonna - The Metamax <chiagozie1999@gmail.com>
2022-08-25 19:22:25 +0100

    Updated Character Controller

---
+++ BGJ 2022/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 5.0f;
+    public float acceleration = 5.0f;
+    public float deceleration = 5.0f;
+
+    Vector2 movementAxis;
+    Vector2 movement;
+    Rigidbody2D rb;
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }   
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement(){
+        movementAxis.x = Input.GetAxisRaw("Horizontal");
+        movementAxis.y = Input.GetAxisRaw("Vertical");
+        movementAxis.Normalize();
+
+        if(movementAxis.magnitude == 0){
+            // deceleration
+            movement.x = Mathf.Lerp(movement.x, 0, deceleration * Time.deltaTime);
+            movement.y = Mathf.Lerp(movement.y, 0, deceleration * Time.deltaTime);
+        }else{
+            // acceleration
+            if(movement.magnitude < maxSpeed){
+                movement.x = Mathf.Lerp(movement.x, movementAxis.x * maxSpeed, acceleration * Time.deltaTime);
+                movement.y = Mathf.Lerp(movement.y, movementAxis.y * maxSpeed, acceleration * Time.deltaTime);
+
+                //movement.x += movementAxis.x * acceleration * Time.deltaTime;
+                //movement.y += movementAxis.y * acceleration * Time.deltaTime;
+            }
+        }
+        Debug.Log(movement);
+        rb.MovePosition(rb.position + movement);
+    }
+}
