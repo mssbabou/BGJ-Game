06f5bc202cd1f7e049314ed5d7d0a0c86f641d42
Chiagozie Ogbonna - The Metamax <chiagozie1999@gmail.com>
2022-08-25 21:17:42 +0100

    Added acceleration and run
    The character can now run as long as the player holds the left shift key

--- BGJ 2022/Assets/Scripts/Player/PlayerMovement.cs
+++ BGJ 2022/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    [SerializeField] private float walkSpeed;
+    [SerializeField] private float walkSpeed = 3f;
+    [Tooltip("This is the amount of force that will be given to the character in order to accelerate. The higher this value is, the faster the player reaches its run/walk speed")]
+    [SerializeField] private float workForce = 1f;
+    [SerializeField] private float runSpeed = 7f;
+
+    private float acceleration;
 
     private Vector2 movementVector;
 
@@ -21,14 +24,25 @@ public class PlayerMovement : MonoBehaviour
         float verticalMovement = Input.GetAxis("Vertical");
         movementVector = new Vector2(horizontalMovement, verticalMovement).normalized;
 
-        if (movementVector.x > 0f){}
+        if (movementVector.magnitude > 0f)
+            acceleration = Mathf.Lerp(acceleration, GetSpeed(), workForce * Time.deltaTime);        //gradually reach the walkspeed or runspeed over time
+        else
+            acceleration = 0f;
+
+        /*if (movementVector.x > 0f){}
             // Flip
         else if (movementVector.x < 0f){}
-            // Flip
+            // Flip*/
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = walkSpeed * movementVector;
+        rb.velocity = acceleration * movementVector;
     }
+
+    /// <summary>
+    /// Returns the speed based on player input
+    /// </summary>
+    /// <returns>If the player presses and holds onto the left shift key while moving, the character should run</returns>
+    private float GetSpeed() => Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
 }
